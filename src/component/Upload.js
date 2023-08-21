88c9dcd81762bedcac9248b783416721cd2122df
Chandan Nath <46443346+chandannath500@users.noreply.github.com>
2023-08-21 09:39:46 +0530

    Update Upload.js

--- src/component/Upload.js
+++ src/component/Upload.js
@@ -1,35 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { InboxOutlined } from '@ant-design/icons';
 import { message, Upload } from 'antd';
+
 const { Dragger } = Upload;
-const props = {
-  name: 'file',
-  multiple: true,
-  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-  onChange(info) {
-    const { status } = info.file;
-    if (status !== 'uploading') {
-      console.log(info.file, info.fileList);
+
+const App = ({ main, sub, onFileUpload }) => {
+  const [fileList, setFileList] = useState([]);
+
+  const handleFileChange = ({ file, fileList }) => {
+    if (file.status === 'done') {
+      message.success(`${file.name} file uploaded successfully.`);
+    } else if (file.status === 'error') {
+      message.error(`${file.name} file upload failed.`);
     }
-    if (status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === 'error') {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  },
-  onDrop(e) {
-    console.log('Dropped files', e.dataTransfer.files);
-  },
+
+    const updatedFileList = fileList.map((f) => {
+      if (f.status === 'done') {
+        return {
+          ...f,
+          filename: f.name, // Add filename property
+        };
+      }
+      return f;
+    });
+
+    setFileList(updatedFileList);
+    onFileUpload(updatedFileList);
+  };
+
+  const props = {
+    name: 'file',
+    multiple: true,
+    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    fileList,
+    onChange: handleFileChange,
+    onDrop(e) {
+      console.log('Dropped files', e.dataTransfer.files);
+    },
+  };
+
+  return (
+    <Dragger {...props}>
+      <p className="ant-upload-drag-icon">
+        <InboxOutlined />
+      </p>
+      <p className="ant-upload-text">{main}</p>
+      <p className="ant-upload-hint">{sub}</p>
+    </Dragger>
+  );
 };
-const App = () => (
-  <Dragger {...props}>
-    <p className="ant-upload-drag-icon">
-      <InboxOutlined />
-    </p>
-    <p className="ant-upload-text">Browse Files</p>
-    <p className="ant-upload-hint">
-      Drag and drop files here
-    </p>
-  </Dragger>
-);
+
 export default App;
